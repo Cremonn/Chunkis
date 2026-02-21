890dda0e4282153478f8c4d441c0a4ca15aa7233
Liparakis <stiliano446@gmail.com>
2026-02-21 19:47:24 +0200

    Init

--- fabric/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-val loader_version = property("loader_version") as String
-val fabric_version = property("fabric_version") as String
-
-dependencies {
-    implementation(project(path = ":core", configuration = "namedElements"))
-
-    modImplementation("net.fabricmc:fabric-loader:${loader_version}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabric_version}")
-}
