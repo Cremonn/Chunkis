0300b236846b2c89873fd9a3eac07d724dffbe6e
Liparakis <stiliano446@gmail.com>
2026-03-02 20:21:27 +0200

    feat: Scaffolding for neoforge and forge.

---
+++ forge/src/main/java/io/liparakis/chunkis/forge/ChunkisMod.java
@@ -0,0 +1,11 @@
+package io.liparakis.chunkis.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("chunkis")
+public class ChunkisMod {
+
+    public ChunkisMod() {
+        // TODO: register Forge event bus listeners and adapt core logic
+    }
+}
